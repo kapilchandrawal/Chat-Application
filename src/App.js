fafbcb6255796496880f366872432d8ae248632c
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2023-07-24 23:57:38 +0530

    Added new ChatFeed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       projectID="02bf996b-77b6-4b31-bec3-560912796e98"
       userName="JSMastery"
       userSecret="123123"
+      renderChatFeed={(ChatAppProps)=> <ChatFeed {...ChatAppProps} />}
     />
   );
 };
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,30 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+const ChatFeed = (props) => {
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+//   console.log(chat, userName, messages);
+
+  const renderMessages  = () => {
+    const keys = Object.keys(messages);
+    console.log(keys);
+    return keys.map((key, index)=>{
+        const message = messages[key];
+        const lastMessageKey = index === 0 ? null : keys[index - 1]
+        const isMyMessage = userName === message.sender.username;
+
+        return (
+            <div key={`msg_${index}`} style={{width: '100%'}}>
+                <div className="message-block">
+                    {isMyMessage ? <MyMessage />:<TheirMessage />} 
+                </div>
+            </div>
+        )
+    })
+  }
+  renderMessages()
+  return <div>ChatFeed</div>;
+};
+export default ChatFeed;
