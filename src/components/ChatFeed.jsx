50485e7094eafd1e2a1a252abfa9bf7a06c8b942
kapilchandrawal <77915124+kapilchandrawal@users.noreply.github.com>
2023-07-25 23:09:53 +0530

    Created ChatFeed page with logic

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -5,26 +5,47 @@ import TheirMessage from "./TheirMessage";
 const ChatFeed = (props) => {
   const { chats, activeChat, userName, messages } = props;
   const chat = chats && chats[activeChat];
-//   console.log(chat, userName, messages);
+  //   console.log(chat, userName, messages);
 
-  const renderMessages  = () => {
+  const renderMessages = () => {
     const keys = Object.keys(messages);
     console.log(keys);
-    return keys.map((key, index)=>{
-        const message = messages[key];
-        const lastMessageKey = index === 0 ? null : keys[index - 1]
-        const isMyMessage = userName === message.sender.username;
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = userName === message.sender.username;
 
-        return (
-            <div key={`msg_${index}`} style={{width: '100%'}}>
-                <div className="message-block">
-                    {isMyMessage ? <MyMessage />:<TheirMessage />} 
-                </div>
-            </div>
-        )
-    })
-  }
-  renderMessages()
-  return <div>ChatFeed</div>;
+      return (
+        <div key={`msg_${index}`} style={{ width: "100%" }}>
+          <div className="message-block">
+            {isMyMessage ? <MyMessage /> : <TheirMessage />}
+          </div>
+          <div
+            className="read-receipts"
+            style={{
+              marginRight: isMyMessage ? "18px" : "0px",
+              marginLeft: isMyMessage ? "0px" : "68px",
+            }}
+          >
+            read-receipts
+          </div>
+        </div>
+      );
+    });
+  };
+  if (!chat) return "Loading...";
+  return (
+    <div className="chat-feed">
+      <div className="chat-title">{chat.title}</div>
+      <div className="chat-subtitle">
+        {chat.people.map((person) => `${person.person.username}`)}
+      </div>
+      {renderMessages()}
+      <div style={{ height: "100px" }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
+      </div>
+    </div>
+  );
 };
 export default ChatFeed;
